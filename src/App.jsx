c56a460f57e867e49ebd624ac8a9ab9bde6e2313
Sopan Roy <sopan619@gmail.com>
2023-07-12 21:25:34 +0530

    infinite search loop working need to set clearinterveal

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
+import { generate} from "random-words";
 
 function App() {
   // This function will be called upon button click on our website 
   const openTab = () =>{
-    // Opens a new tab and performs the search 
-    let bing;
-    bing = window.open("https://www.bing.com/search?q=g", "_blank");
-    // console.log("Hello")
-    // Closes the opened window after 3 seconds
-     setTimeout(()=>{
-      //  console.log("3 seconds later")
-       bing.close();
-     },3000);
-
+      let bing;
+      let text = generate();
+      bing = window.open("https://www.bing.com/search?q=" + text, "_blank");
+      setTimeout(()=>{
+         bing.close();
+       },3500);
   }
-  return (
-    
-    <div className="wrapper mb-20 flex flex-col">
 
+  const stopSearch = ()=>{
+    openTab();
+   setInterval(openTab,5000)
+  }
+
+  return (
+    <div className="wrapper mb-20 flex flex-col">
       <div className="text-center my-5">
         <h1 className="text-3xl font-bold text-orange-900">
           We will make our app here.
           </h1>
       </div>
       <div className="mx-auto">
-        <button className="bg-yellow-500 w-fit py-2 px-3 rounded-lg te" onClick={openTab}>Click Me</button>
+        <button className="bg-yellow-500 w-fit py-2 px-3 rounded-lg" onClick={stopSearch}>Click Me</button>
       </div>
     </div>
   );
