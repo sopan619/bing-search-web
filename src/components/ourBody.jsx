2363b29f85c7eb2c05bbdda3d7337f83c917e4e1
Sopan Roy <sopan619@gmail.com>
2023-07-22 11:16:13 +0530

    Some redesign done to the page no changes to functionality

--- src/components/ourBody.jsx
+++ src/components/ourBody.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {generate} from "random-words";
-import image from '../pic1.png'
+import image from '../pic.jpg'
+import search from '../search.png'
 
 
 const ourBody = () => {
@@ -33,27 +34,32 @@ const ourBody = () => {
   return (
     <div className="wrapper flex flex-col">
       <div className="text-center my-4">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-josefin text-stone-700">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-josefin text-[#CBE4DE]">
           Earn Microsoft Reward Points Automatically
         </h1>
       </div>
-      <div className='image sm:w-[80vw] md:w-[75vw] lg:w-[30vw] mx-auto'>
+      <div className='image md:w-[70vw] lg:w-[30vw] mx-auto'>
         <img src={image} alt="man adding money to wallet" className='rounded-xl shadow-sm' />
       </div>
       <div className="mx-auto items-center flex flex-col gap-5">
-        <label htmlFor="myInp" className="text-[#141413] text-xl md:text-2xl lg:text-3xl font-fasthand">How many Searches you want to perform?</label>
-        <input type="number" id="myInp" className="w-72 py-2 rounded-lg bg-[#f59a51]/60 outline-2 outline outline-offset-2 text-center text-slate-900 text-lg font-josefin font-bold"/>
+        <label htmlFor="myInp" className="text-[#CBE4DE] text-xl md:text-2xl lg:text-3xl font-fasthand">How many Searches you want to perform?</label>
+        <input type="number" id="myInp" className="w-72 py-2 rounded-lg bg-[#64CCC5]/50 outline-2 outline outline-offset-2 text-center text-slate-800 text-lg font-josefin font-bold"/>
         
-        <p id='note' className='text-[#141413]/60 font-josefin text-sm w-3/4 text-center'>Note: In Mobile, all you need is 20 searches to earn maximum points.</p>
+        <p id='note' className='text-[#FFD95A]/60 font-josefin text-sm w-3/4 text-center'>Note: In Mobile, all you need is 20 searches to earn maximum points.</p>
         <div>
           
-        <button className="bg-[#446bd0] hover:bg-[#446bd0]/50 py-2 rounded-lg text-lg text-stone-900 font-josefin font-bold w-60" onClick={stopSearch}>Search</button>
+        <button className="bg-[#0E8388] hover:bg-[#0E8388]/50 py-2 rounded-lg text-lg text-slate-800 font-josefin font-bold w-60 flex items-center" onClick={stopSearch}>
+          <p className='ml-[30%] w-fit'>Search</p>
+          <span className='ml-[7%] w-fit'>
+        <img src={search} alt="search icon" className='w-6' />
+        </span>
+          </button>
       </div>
       <footer className='sm:mt-20 mt-14 flex flex-col items-center'>
-        <p className='text-xs opacity-40'>Image by redgreystock on Freepik</p>
-        <p className='text-xs opacity-40'>Made by</p>
-        <p className='text-xs opacity-60'>Sopan Roy</p>
-        <p className='text-xs opacity-80'>💙</p>
+        <p className='text-xs text-[#FFF7D4] opacity-40'>Image by <a href="https://pixabay.com/users/mohamed_hassan-5229782/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6039919">Mohamed Hassan</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6039919">Pixabay</a></p>
+        <p className='text-xs text-[#FFF7D4] opacity-40'>Made by</p>
+        <p className='text-xs text-[#FFF7D4] opacity-60'>Sopan Roy</p>
+        <p className='text-xs opacity-70'>💛</p>
       </footer>
       </div>
       
--- src/components/ourNav.jsx
+++ src/components/ourNav.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <nav className='bg-[#446bd0] h-16 w-screen flex items-center'> 
-      <h1 className='mx-auto text-[#f3faff] font-rubik text-3xl md:text-4xl lg:text-5xl'>BING AUTO SEARCH</h1>
+    <nav className='bg-[#2C3333] h-16 w-screen flex items-center'> 
+      <h1 className='mx-auto text-lime-100 font-rubik text-3xl md:text-4xl lg:text-5xl'>BING AUTO SEARCH</h1>
     </nav>
     </>
   )
